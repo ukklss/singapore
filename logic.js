4dd4e9d48d901e574613183e97b0ed216972ac1c
lei <lei@teaspoonmacbook.lan>
2023-02-14 23:09:58 -0800

    addingfiles

---
+++ logic.js
@@ -0,0 +1,178 @@
+// const map = L.map('map', {
+//   center: [1.3521, 103.8198],
+//   zoom: 11.5
+// });
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href=https://www.openstreetmap.org/copyright>OpenStreetMap</a> contributors' }).addTo(map);
+ 
+// const basicBeachIcon = L.icon({iconUrl: 'hotel.svg',iconSize: [40, 40],
+// });
+
+ 
+
+// const marker1 = L.marker([1.2838, 103.8591], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker2 = L.marker([1.2816, 103.8636], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker3 = L.marker([1.3138, 103.8159], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker4 = L.marker([1.2893, 103.8631], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker5 = L.marker([1,3048, 103.8318], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker6 = L.marker([1.2818, 103.8439], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+// const marker7 = L.marker([1,352083, 103.819], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+// const marker8 = L.marker([1.2902, 103.8515], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker9 = L.marker([1.2494, 103.8303], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// // Food
+
+// const marker10 = L.marker([1.2863, 103.8049], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker11 = L.marker([1.3215, 103.9241], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker12 = L.marker([1.3526, 103.9447], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker13 = L.marker([1.2941, 103.8320], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker14 = L.marker([1.292371830061868, 103.85987245546445], {icon: basicBeachIcon})
+
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker15 = L.marker([1.3511280453513634, 103.87187075546444], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker16 = L.marker([1.3047010983105214, 103.83604021128308], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker17 = L.marker([1.3041491553898554, 103.83248798934842], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker18 = L.marker([1.3145158013913716, 103.85278448430066], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker19 = L.marker([1.303628142062989, 103.85888912472431], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker20 = L.marker([1.2785629172136237, 103.84371652883583], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker21 = L.marker([1.2805365307536163, 103.84476795474643], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker22 = L.marker([1.3168343890859253, 103.80552652472419], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker23 = L.marker([1.3084228417116364, 103.88584449773917], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker24 = L.marker([1.3494253903773339, 103.84992315541069], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker25 = L.marker([1.3242237405447168, 103.81412231123171], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker26 = L.marker([1.3112174976707525, 103.7882870400675], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker27 = L.marker([1.2852664127104851, 103.83285434006758], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker28 = L.marker([1.2858722134470977, 103.84199134990234], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker29 = L.marker([1.3380435890447093, 103.88885490477573], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker30 = L.marker([1.3556159706916826, 103.83632994006739], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker31 = L.marker([1.3099635457294077, 103.90201362472423], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker32 = L.marker([1.279138043837685, 103.84404781308257], {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker33 = L.marker([1.2795835438056944, 103.84672225541094], {icon: basicBeachIcon})
+
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker34 = L.marker([1.2860803986513647, 103.84589033644124, {icon: basicBeachIcon})
+//   .bindPopup('Marina Bay Sands')
+//   .addTo(map);
+
+// const marker28 = L.marker([]).addTo(map);
+
+// const marker28 = L.marker([]).addTo(map);
+
+
+
+
+
+const map = L.map('map', {
+  center: [1.3521, 103.8198],
+  zoom: 11.5
+});
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href=https://www.openstreetmap.org/copyright>OpenStreetMap</a> contributors' }).addTo(map);
+ 
+const basicBeachIcon = L.icon({iconUrl: 'hotel.svg',iconSize: [30, 30],
+});
+const bakeryIcon = L.icon({iconUrl: 'bakery.svg',iconSize: [30, 30],
+});
+ 
+
+const marker1 = L.marker([1.2838, 103.8591], {icon: basicBeachIcon})
+  .bindPopup('Marina Bay Sands')
+  .addTo(map);
+
+const marker23 = L.marker([1.3084228417116364, 103.88584449773917], {icon: bakeryIcon})
+  .bindPopup('Mother Dough Bakery')
+  .addTo(map);
