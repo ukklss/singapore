2fb5bf7305b025f81b864d3f5d6d1aedf75705e7
lei <lei@teaspoonmacbook.lan>
2023-02-15 18:31:16 -0800

    Update logic.js

--- logic.js
+++ logic.js
@@ -167,38 +167,38 @@ const marker21 = L.marker([1.28053, 103.8447], {icon: marketIcon})
   .bindPopup('<a href="https://goo.gl/maps/5tdUeD7miG4mxcuM9"><b>Maxwell Food Centre</b></a> <br /> Tian Tian Hainanese Chicken Rice <br /> Fu Shun Roasted Meat Specialist')
   .addTo(map);
 
-// const marker22 = L.marker([1.3168, 103.8055], {icon: bakeryIcon})
-//   .bindPopup('<a href="https://goo.gl/maps/5BGcu2mJe7V9YTjH8">Bakery Brera & Fine Foods</a>')
-//   .addTo(map);
+const marker22 = L.marker([1.3168, 103.8055], {icon: bakeryIcon})
+  .bindPopup('<a href="https://goo.gl/maps/5BGcu2mJe7V9YTjH8">Bakery Brera & Fine Foods</a>')
+  .addTo(map);
  
-// const marker23 = L.marker([1.3084, 103.8858], {icon: marketIcon})
-//   .bindPopup('<a href="https://goo.gl/maps/5PXm3mbKWPZW9g9z8"><b>Old Airport Road Centre</b></a> <br /> Dong Ji Fried Kway Teow')
-//   .addTo(map);
+const marker23 = L.marker([1.3084, 103.8858], {icon: marketIcon})
+  .bindPopup('<a href="https://goo.gl/maps/5PXm3mbKWPZW9g9z8"><b>Old Airport Road Centre</b></a> <br /> Dong Ji Fried Kway Teow')
+  .addTo(map);
 
-// const marker24 = L.marker([1.3494, 103.8499], {icon: marketIcon})
-//   .bindPopup('<a href="https://goo.gl/maps/Hkhn8zFjMvj2wrwA6">Kim San Leng Food Centre Bishan</a>')
-//   .addTo(map);
+const marker24 = L.marker([1.3494, 103.8499], {icon: marketIcon})
+  .bindPopup('<a href="https://goo.gl/maps/Hkhn8zFjMvj2wrwA6">Kim San Leng Food Centre Bishan</a>')
+  .addTo(map);
 
-// const marker25 = L.marker([1.3242, 103.8141], {icon: marketIcon})
-//   .bindPopup('<a href="https://goo.gl/maps/Vo6CmCa3Ez4JHWrg7"><b>Adam Road Centre</b></a> <br /> Selera Rasa Nasi Lemak <br /> Bahrakath Mutton Soup King ')
-  // .addTo(map);
+const marker25 = L.marker([1.3242, 103.8141], {icon: marketIcon})
+  .bindPopup('<a href="https://goo.gl/maps/Vo6CmCa3Ez4JHWrg7"><b>Adam Road Centre</b></a> <br /> Selera Rasa Nasi Lemak <br /> Bahrakath Mutton Soup King ')
+  .addTo(map);
 
-// const marker26 = L.marker([1.3112174976707525, 103.7882870400675], {icon: basicBeachIcon})
-//   .bindPopup('Marina Bay Sands')
-//   .addTo(map);
+const marker26 = L.marker([1.3112174976707525, 103.7882870400675], {icon: basicBeachIcon})
+  .bindPopup('Marina Bay Sands')
+  .addTo(map);
 
-// const marker27 = L.marker([1.2852664127104851, 103.83285434006758], {icon: basicBeachIcon})
-//   .bindPopup('Marina Bay Sands')
-//   .addTo(map);
+const marker27 = L.marker([1.2852664127104851, 103.83285434006758], {icon: basicBeachIcon})
+  .bindPopup('Marina Bay Sands')
+  .addTo(map);
 
-// const marker28 = L.marker([1.2858722134470977, 103.84199134990234], {icon: basicBeachIcon})
-//   .bindPopup('Marina Bay Sands')
-//   .addTo(map);
+const marker28 = L.marker([1.2858722134470977, 103.84199134990234], {icon: basicBeachIcon})
+  .bindPopup('Marina Bay Sands')
+  .addTo(map);
 
-// const marker29 = L.marker([1.3380435890447093, 103.88885490477573], {icon: basicBeachIcon})
-//   .bindPopup('Marina Bay Sands')
-//   .addTo(map);
+const marker29 = L.marker([1.3380435890447093, 103.88885490477573], {icon: basicBeachIcon})
+  .bindPopup('Marina Bay Sands')
+  .addTo(map);
 
-// const marker30 = L.marker([1.3556159706916826, 103.83632994006739], {icon: basicBeachIcon})
-//   .bindPopup('Marina Bay Sands')
-//   .addTo(map);
+const marker30 = L.marker([1.3556159706916826, 103.83632994006739], {icon: basicBeachIcon})
+  .bindPopup('Marina Bay Sands')
+  .addTo(map);
